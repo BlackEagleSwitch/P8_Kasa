6af7f651a90bef644f826388912ca9531c1fa55e
Corentin Beaudet <corentin.abbruzzese14@gmail.com>
2023-08-21 01:56:23 +0200

    add component to propos pages

--- src/pages/A-Propos/index.jsx
+++ src/pages/A-Propos/index.jsx
@@ -1,7 +1,30 @@
+import Banner from "./../../components/Banner"
+import Collapse from "../../components/Collapse"
+
+import propos from "./../../data/propos"
+import imgBanner from "./../../assets/background_2.png"
+
+import "./../../styles/Banner.css"
+
 const APropos = () => {
     return (
         <div className="a_propos">
-            
+            <Banner>
+                <div className="banner-kasa">
+                    <img src={imgBanner} alt="paysage" />            
+                    <p></p>
+                </div>
+            </Banner>
+            <ul>
+                {propos.map(({ title, texte }) => (
+                    <div key={title}>
+                        <Collapse 
+                            title={title}
+                            texte={texte}
+                        />
+                    </div>
+                ))}
+            </ul>
         </div>
     )
 }
