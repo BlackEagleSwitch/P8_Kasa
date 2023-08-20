08019114fb321c26c80633609aa9bf9996749c7d
Corentin Beaudet <corentin.abbruzzese14@gmail.com>
2023-08-21 01:58:18 +0200

    add error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom/dist"
 import "./../../styles/Error.css"
 
 function Error() {
     return (
         <div className="error">
-            
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d’accueil</Link>
         </div>
     )
 }
